615b86ea7b7a37a7ae42d16c869c4f718ca8cf4a
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-07-11 04:29:53 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,11 +7,12 @@
 # }
 
 def select_winner(passengers)
-  inner = ""
+  winner = ""
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
       winner = name
     end
   end
 
+  winner  
 end
